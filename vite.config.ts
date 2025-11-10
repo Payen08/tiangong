cd6de4bdf2786f56edd7483eea8cc15d7da450d8
Payen08 <101703738+Payen08@users.noreply.github.com>
2025-11-10 21:59:44 +0800

    Add base path configuration to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  // --- 在这里添加 base 属性 ---
+  base: '/tiangong/', 
+  // --------------------------
+  
   plugins: [
     react(),  // 使用默认配置，避免babel-plugin-react-dev-locator依赖问题
     tsconfigPaths()
